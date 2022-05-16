12e94496fd00fe403a0f754ed05063194305ad5d
Sean Bikkes <bixkitts@gmail.com>
2022-05-16 18:07:13 +0200

    Made listening decent

--- src/listen.cpp
+++ src/listen.cpp
@@ -7,17 +7,25 @@
 #include <netdb.h>
 #include "clientObject.h"
 
-int listenForData(char *buffer, uint16_t bufsize, Client &client) 
+namespace netlib
 {
-    int n;
-    socklen_t len = sizeof(client.Socket);
-    while(1)
+
+    int listenForData(char *buffer, uint16_t bufsize, Client &client)    //Should be called on it's own thread as the while loop is blocking 
     {
-        n = recvfrom(client.sockfd, buffer, bufsize, MSG_WAITALL, ( struct sockaddr *)&client.Socket, &len);
-        buffer[n] = '\0';
-        printf("Received a datagram : %s\n", buffer);
+        client.bListen = 1;
+        int n;
+        socklen_t len = sizeof(client.Socket);
+        while(client.bListen == 1)
+        {
+            n = recvfrom(client.sockfd, buffer, bufsize, MSG_WAITALL, ( struct sockaddr *)&client.Socket, &len);
+            buffer[n] = '\0';
+        }
+
+        return 0;
     }
-    return 0;
+
+    void stopListen(Client &client)
+    {
+        client.bListen = 0;
+    }
 }
-
-
--- src/makesock.cpp
+++ src/makesock.cpp
@@ -6,32 +6,34 @@
 #include <strings.h>
 #include <unistd.h>
 #include "clientObject.h"
-
-Client* createClient(char *ip, u_short port)
+namespace netlib
 {
-    Client* client = new Client;
- 	 
-    if ( (client->sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); //perror is a system call to write an error.
-        exit(EXIT_FAILURE);
-    }   
-    //"bzero" is a syscall to zero all the bytes
-    bzero((char *) &client->Socket, sizeof(client->Socket));
-  
-    //filling in the data of the server's ip.
-    client->Socket.sin_family = AF_INET;
-    client->Socket.sin_port = htons(port);
-    inet_pton(AF_INET, ip, &client->Socket.sin_addr);
-    //bind the socket "sockfd" to serverthe address and port in the sockaddr struct "server"
-    bind(client->sockfd, (struct sockaddr *) &client->Socket, sizeof(client->Socket));
+    Client* createClient(char *ip, u_short port)
+    {
+        Client* client = new Client;
+         
+        if ( (client->sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+            perror("socket creation failed"); //perror is a system call to write an error.
+            exit(EXIT_FAILURE);
+        }   
+        //"bzero" is a syscall to zero all the bytes
+        bzero((char *) &client->Socket, sizeof(client->Socket));
+      
+        //filling in the data of the client's ip.
+        client->Socket.sin_family = AF_INET;
+        client->Socket.sin_port = htons(port);
+        inet_pton(AF_INET, ip, &client->Socket.sin_addr);
+        //bind the socket "client->sockfd" to the address and port in the sockaddr struct "client->Socket"
+        bind(client->sockfd, (struct sockaddr *) &client->Socket, sizeof(client->Socket));
 
-    return client;
-}
+        return client;
+    }
 
-int removeClient(Client* client)
-{
-	close(client->sockfd);
-	delete client;
+    int removeClient(Client* client)
+    {
+        close(client->sockfd);
+        delete client;
 
-	return 0;
+        return 0;
+    }
 }
