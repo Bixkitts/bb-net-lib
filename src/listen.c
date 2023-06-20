7bd68f31a077f1e58c40fa62961b034408922a26
Sean Bikkes <bixkitts@gmail.com>
2023-06-20 19:41:54 +0200

    made TCP connections listen and close properly

--- src/listen.c
+++ src/listen.c
@@ -12,7 +12,7 @@
 #include "listen.h"
 #include "socketsNetLib.h"
 
-static int receiveTCPpackets(char *buffer, const uint16_t bufsize, Client* remotehost, const Client *localhost, void (*packet_handler)(char*, uint16_t, Client*)) ;
+static int receiveTCPpackets(char *buffer, const uint16_t bufsize, Client* remotehost, Client *localhost, void (*packet_handler)(char*, uint16_t, Client*)) ;
 
 
 int listenForUDP(char *buffer, const uint16_t bufsize, Client *localhost, Client *remotehost, void (*packet_handler)(char*, uint16_t, Client*))    //Should be called on it's own thread as the while loop is blocking 
@@ -45,8 +45,10 @@ int listenForUDP(char *buffer, const uint16_t bufsize, Client *localhost, Client
         packet_handler(buffer, bufsize, remotehost);
     }
 
-    unsetListening(localhost);
-    unsetListening(remotehost);
+    if(isListening(localhost))
+        unsetListening(localhost);
+    if(isListening(remotehost))
+        unsetListening(remotehost);
 
     return SUCCESS;
 }
@@ -63,33 +65,32 @@ int listenForTCP(char *buffer, const uint16_t bufsize, Client *localhost, Client
     setListening(localhost);
     setListening(remotehost);
     // Still need to patch this up for multiple conections
-    while(isListening(localhost) && isListening(remotehost))
+    if (FAILURE(listen(sockfd, SOCK_BACKLOG))) 
     {
-        if (FAILURE(listen(sockfd, SOCK_BACKLOG))) 
-        {
-            printf("Error: listen() failed with errno %d: %s\n", errno, strerror(errno));
-            return ERROR;
-        }
-
-        socklen_t len = sizeof(remotehost->address);
-        struct sockaddr* remoteAddress = ( struct sockaddr *)&remotehost->address;
-
-        // Accept incoming  from remote host
-        setSocket(remotehost, createSocket(accept(sockfd, remoteAddress, &len)));
-
-        // Receive TCP packets from the connected remote host
-        receiveTCPpackets(buffer, bufsize, remotehost, localhost, packet_handler);
+        printf("Error: listen() failed with errno %d: %s\n", errno, strerror(errno));
+        return ERROR;
     }
+
+    socklen_t len = sizeof(remotehost->address);
+    struct sockaddr* remoteAddress = ( struct sockaddr *)&remotehost->address;
+
+    // Accept incoming  from remote host
+    setSocket(remotehost, createSocket(accept(sockfd, remoteAddress, &len)));
+
+    // Receive TCP packets from the connected remote host
+    receiveTCPpackets(buffer, bufsize, remotehost, localhost, packet_handler);
 
     closeSocket(sockfd);
 
-    unsetListening(localhost);
-    unsetListening(remotehost);
+    if(isListening(localhost))
+        unsetListening(localhost);
+    if(isListening(remotehost))
+        unsetListening(remotehost);
 
     return SUCCESS;
 }
 
-static int receiveTCPpackets(char *buffer, const uint16_t bufsize, Client* remotehost, const Client *localhost, void (*packet_handler)(char*, uint16_t, Client*)) 
+static int receiveTCPpackets(char *buffer, const uint16_t bufsize, Client* remotehost, Client *localhost, void (*packet_handler)(char*, uint16_t, Client*)) 
 {
     int64_t numBytes;
     // Receive data continuously until client closes 
@@ -97,10 +98,22 @@ static int receiveTCPpackets(char *buffer, const uint16_t bufsize, Client* remot
     {
         // Receive TCP packet from client and store in buffer
         numBytes = recv(getSocket(remotehost), buffer, bufsize, 0);
-        buffer[numBytes] = '\0'; // Null terminate the received data
-
-        packet_handler(buffer, bufsize, remotehost);
+        if (numBytes == 0)
+        {
+            (void)printf("\nClient negotiated connection close/timeout\n");
+            unsetListening(localhost);
+        }
+        else if (numBytes == -1)
+        {
+            (void)printf("\nError: recv() failed with errno %d: %s\n", errno, strerror(errno));
+            unsetListening(localhost);
+            return ERROR;
+        }
+        else
+        {
+            buffer[numBytes] = '\0'; // Null terminate the received data
+            packet_handler(buffer, bufsize, remotehost);
+        }
     }
-    closeSocket(getSocket(remotehost));
     return SUCCESS;
 }
